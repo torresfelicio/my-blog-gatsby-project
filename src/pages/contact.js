6d4b531499d423bd7a66d9f6f12477da16447d1f
torresfelicio <lucastorresfellicio@gmail.com>
2020-03-24 17:52:19 -0300

    quase finalizado, falta alguns pequenos detalhes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,13 @@
 import React from  'react'
 
 import Layout from '../components/layout'
-
 const ContactPage = () => {
     return (
         <Layout>
             <h1>Contact</h1>
-            <p> Meu melhor email está aqui para entrares em contato.</p>
+            <ul>
+                <li>Twitter</li>
+            </ul>
         </Layout>
 
     )
