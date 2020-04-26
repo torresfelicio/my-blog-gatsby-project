b445db2e8d2706d9c47c43012c69cd68c80447af
torresfelicio <lucastorresfellicio@gmail.com>
2020-04-25 22:59:37 -0300

    finalizado

--- data/config.js
+++ data/config.js
@@ -4,7 +4,7 @@ const config = {
   siteTitleShort: 'Lucas Torres',
   siteRss: "/rss.xml",
   siteImage: "https://avatars1.githubusercontent.com/u/18267941?v=4",
-  googleAnalyticsID: "**-********-*",
+  googleAnalyticsID: "",
   themeColor: "#070719",
   backgroundColor: "#FFFAFA",
   siteUrl: "https://lucastorres.netlify.com",
