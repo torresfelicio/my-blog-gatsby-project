f21460add9f10f27d0ab4d0194ca9d074cadc38e
torresfelicio <lucastorresfellicio@gmail.com>
2020-11-28 18:03:52 -0300

    feat: config gatsby with plugin prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,32 @@ require("dotenv").config({
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-source-prismic`,
+      resolve: 'gatsby-source-prismic',
       options: {
-        repositoryName: `blog-do-lucis`,
-        accessToken: `${process.env.PRISMIC_KEY}`,
+        repositoryName: 'my-blog-gatsby-project',
+        accessToken: 'MC5YOEt4N2hJQUFDWUFnMllr.77-977-977-977-9TC7vv73vv70iVmwr77-9eO-_vRLvv73vv73vv71WZe-_vXdf77-9Qe-_ve-_ve-_ve-_vSfvv70',
+        releaseID: 'example-eiyaingiefahyi7z',
         linkResolver: ({ node, key, value }) => (post) => `/${post.uid}`,
         schemas: {
           post: require('./src/schemas/post.json'),
         },
+        lang: '*',
+        prismicToolbar: true,
+        shouldDownloadImage: ({ node, key, value }) => {
+          // Return true to download the image or false to skip.
+        },
+        imageImgixParams: {
+          auto: 'compress,format',
+          fit: 'max',
+          q: 50,
+        },
+        imagePlaceholderImgixParams: {
+          w: 100,
+          blur: 15,
+          q: 50,
+        },
+        typePathsFilenamePrefix:
+          'prismic-typepaths---your-repo-name',
       },
     },
   ],
