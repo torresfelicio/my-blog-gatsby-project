ca6cd52acb011841a03c04e67008b808131669ca
torresfelicio <lucastorresfellicio@gmail.com>
2020-12-05 17:19:22 -0300

    feat: alter config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,6 @@ module.exports = {
       options: {
         repositoryName: 'my-blog-gatsby-project',
         accessToken: 'MC5YOEt4N2hJQUFDWUFnMllr.77-977-977-977-9TC7vv73vv70iVmwr77-9eO-_vRLvv73vv73vv71WZe-_vXdf77-9Qe-_ve-_ve-_ve-_vSfvv70',
-        releaseID: 'example-eiyaingiefahyi7z',
         linkResolver: ({ node, key, value }) => (post) => `/${post.uid}`,
         schemas: {
           post: require('./src/schemas/post.json'),
@@ -29,8 +28,6 @@ module.exports = {
           blur: 15,
           q: 50,
         },
-        typePathsFilenamePrefix:
-          'prismic-typepaths---your-repo-name',
       },
     },
   ],
