942402d4bdc0365a898a3987c3b91c7e4f827822
torresfelicio <lucastorresfellicio@gmail.com>
2020-12-06 12:48:19 -0300

    fix: alterado gatsby-config para o prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       resolve: `gatsby-source-prismic`,
       options: {
-        repositoryName: `blog-do-lucis`,
-        accessToken: `${process.env.PRISMIC_KEY}`,
+        repositoryName: `${process.env.PRISMIC_REPOSITORY_NAME}`,
+        accessToken: `${process.env.PRISMIC_ACCESS_TOKEN}`,
         linkResolver: ({ node, key, value }) => (post) => `/${post.uid}`,
         schemas: {
           post: require('./src/schemas/post.json'),
