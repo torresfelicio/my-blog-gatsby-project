6a2071a13442db16df9a48b58e0256aa543c2a8c
torresfelicio <lucastorresfellicio@gmail.com>
2020-12-06 15:08:57 -0300

    feat: criado config.js

---
+++ data/config.js
@@ -0,0 +1,13 @@
+const config = {
+    siteTitle: 'Lucas Torres',
+    siteDescription: 'Um blog contendo parte do meu conhecimento em programação.',
+    siteTitleShort: 'Lucas Torres Dev',
+    siteRss: "/rss.xml",
+    siteImage: "https://unavatar.now.sh/github/torresfelicio",
+    googleAnalyticsID: "**-********-*",
+    themeColor: "#1C1C1C",
+    backgroundColor: "#1C1C1C",
+    siteUrl: "https://lucastorres.dev/",
+  };
+  
+  module.exports = config;
